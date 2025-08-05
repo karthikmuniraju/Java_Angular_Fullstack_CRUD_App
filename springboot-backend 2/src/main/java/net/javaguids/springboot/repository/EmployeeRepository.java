fce3bf5f060d7ba6484fef96910d5ca179cf26c7
Karthik M <karthik.m@Karthiks-MacBook-Air.local>
2025-08-05 11:46:50 +0530

    commiting backend code

---
+++ springboot-backend 2/src/main/java/net/javaguids/springboot/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package net.javaguids.springboot.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import net.javaguids.springboot.model.Employee;
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}
